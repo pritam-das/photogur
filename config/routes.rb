840b3e0d430582ca4af580c40008c5dca89a9787
pritam-das <pritam.das@mail.utoronto.ca>
2017-04-07 13:03:50 -0400

    Adding features to edit pictures

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ get 'pictures/new' => 'pictures#new'
 
 get 'pictures/:id' => 'pictures#show', as: 'picture'
 
+get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  patch 'pictures/:id' => "pictures#update"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
